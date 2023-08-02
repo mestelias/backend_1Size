bb25e1b13d6a155732a07d6efbaef56cba36e8d9
Sacha <Your email>
2023-08-02 16:48:36 +0200

    added marques.js files in routes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var marquesRouter = require('./routes/marques')
+
 const fileUpload = require('express-fileupload');
 
 
@@ -27,5 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/marques', marquesRouter)
 
 module.exports = app;
---
+++ routes/marques.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+
+
+const db = mongoose.connection;
+const Marques = db.collection('Marques')
+
+
+//Dans toutes les routes, possibilité de rendre homme/femme ou la categorie haut/bas/chaussure dynamique
+
+//récupère le nom de toutes les marques en bdd
+router.get('/name', async (req, res) => {
+    const marques = await Marques.distinct('name');
+    res.json(marques);
+  });
+
+//récupère les différents types d'une marque 
+router.get('/types', async (req, res) => {
+    const { marque, sexe, categorie } = req.query;
+    const document = await Marques.findOne({ name:marque });
+    const types = Object.keys(document[sexe][categorie]); 
+    res.json(types);
+});
+
+//récupère les différentes tailles d'un type d'une marque 
+router.get('/tailles', async (req, res) => {
+    const { marque, type, sexe, categorie } = req.query;
+    const document = await Marques.findOne({ name: marque });
+    const tailles = Object.keys(document[sexe][categorie][type]);
+    res.json(tailles);
+});
+
+//récupère le tableau de taille correspondant au combo marque/categorie/type/taille
+router.get('/tableau', async (req, res) => {
+    const { marque, categorie, type, taille, sexe } = req.query;
+    const document = await Marques.findOne({ name : marque })
+    const tableauTaille = document[sexe][categorie][type][taille]
+    res.json(tableauTaille)
+})
+
+
+module.exports = router;
