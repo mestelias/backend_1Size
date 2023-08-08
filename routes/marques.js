803d209cee5146794b75eb503149680fb239e0e2
Nicolas <disbiz@outlook.fr>
2023-08-08 18:32:54 +0200

    route screen logos

--- routes/marques.js
+++ routes/marques.js
@@ -1,25 +1,46 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
 
-
-const db = mongoose.connection;
-const Marques = db.collection('Marques')
-
+require('../models/connection');
+const Marque = require('../models/marques');
 
 //Dans toutes les routes, possibilité de rendre homme/femme ou la categorie haut/bas/chaussure dynamique
 
 //récupère le nom de toutes les marques en bdd
 router.get('/names', async (req, res) => {
     const {categorie, sexe} = req.query // Recevoir la catégorie comme un paramètre de requète 
-    const marques = await Marques.distinct('name', { [`${sexe}.${categorie}`] : {$exists: true}})
+    const marques = await Marque.distinct('name', { [`${sexe}.${categorie}`] : {$exists: true}})
     res.json(marques);
   });
+
+// //récupère les logos de toutes les marques en bdd
+router.get('/logos', async (req, res) => {
+    // const {categorie, sexe} = req.query;
+    
+    // Trouvez toutes les marques qui répondent au critère
+    const marques = await Marque.find({name:'Lacoste'}); // 'name image' spécifie les champs à sélectionner
+    console.log(marques)
+    res.json(marques);
+});
+
+//   router.get('/all', async (req, res) => {
+//     try {
+//       const marques = await Marques.find();
+  
+//       const marqueInfo = marques.map(marque => ({ name: marque.name, url: marque.url }));
+      
+//       res.json(marqueInfo);
+//     } catch (error) {
+//       res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des informations des marques.' });
+//     }
+//   });
+  
+  
 
 //récupère les différents types d'une marque 
 router.get('/types', async (req, res) => {
     const { marque, sexe, categorie } = req.query;
-    const document = await Marques.findOne({ name:marque });
+    const document = await Marque.findOne({ name:marque });
     const types = Object.keys(document[sexe][categorie]); 
     res.json(types);
 });
@@ -27,7 +48,7 @@ router.get('/types', async (req, res) => {
 //récupère les différentes tailles d'un type d'une marque 
 router.get('/tailles', async (req, res) => {
     const { marque, type, sexe, categorie } = req.query;
-    const document = await Marques.findOne({ name: marque });
+    const document = await Marque.findOne({ name: marque });
     const tailles = Object.keys(document[sexe][categorie][type]);
     res.json(tailles);
 });
@@ -35,7 +56,7 @@ router.get('/tailles', async (req, res) => {
 //récupère le tableau de taille correspondant au combo marque/categorie/type/taille
 router.get('/tableau', async (req, res) => {
     const { marque, categorie, type, taille, sexe } = req.query;
-    const document = await Marques.findOne({ name : marque })
+    const document = await Marque.findOne({ name : marque })
     const tableauTaille = document[sexe][categorie][type][taille]
     res.json(tableauTaille)
 })
